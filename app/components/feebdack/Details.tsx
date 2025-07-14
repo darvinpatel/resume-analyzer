55aec8dd367ccb53741409eb3c2a7a6f3f3e281a
Darvin <darwindpatel@gmail.com>
2025-07-14 12:24:12 +0200

    refactor: feedback details component

--- app/components/feebdack/Details.tsx
+++ app/components/feebdack/Details.tsx
@@ -6,76 +6,6 @@ import {
   AccordionItem,
 } from "../Accordion";
 
-const Details = ({ feedback }: { feedback: Feedback }) => {
-  return (
-    <div className="flex flex-col gap-4 w-full">
-      <Accordion>
-        <AccordionItem id="tone-style">
-          <AccordionHeader itemId="tone-style">
-            <CategoryHeader
-              title="Tone & Style"
-              categoryScore={feedback.toneAndStyle.score}
-            />
-          </AccordionHeader>
-          <AccordionContent itemId="tone-style">
-            <CategoryContent tips={feedback.toneAndStyle.tips} />
-          </AccordionContent>
-        </AccordionItem>
-        <AccordionItem id="content">
-          <AccordionHeader itemId="content">
-            <CategoryHeader
-              title="Content"
-              categoryScore={feedback.content.score}
-            />
-          </AccordionHeader>
-          <AccordionContent itemId="content">
-            <CategoryContent tips={feedback.content.tips} />
-          </AccordionContent>
-        </AccordionItem>
-        <AccordionItem id="structure">
-          <AccordionHeader itemId="structure">
-            <CategoryHeader
-              title="Structure"
-              categoryScore={feedback.structure.score}
-            />
-          </AccordionHeader>
-          <AccordionContent itemId="structure">
-            <CategoryContent tips={feedback.structure.tips} />
-          </AccordionContent>
-        </AccordionItem>
-        <AccordionItem id="skills">
-          <AccordionHeader itemId="skills">
-            <CategoryHeader
-              title="Skills"
-              categoryScore={feedback.skills.score}
-            />
-          </AccordionHeader>
-          <AccordionContent itemId="skills">
-            <CategoryContent tips={feedback.skills.tips} />
-          </AccordionContent>
-        </AccordionItem>
-      </Accordion>
-    </div>
-  );
-};
-
-export default Details;
-
-const CategoryHeader = ({
-  title,
-  categoryScore,
-}: {
-  title: string;
-  categoryScore: number;
-}) => {
-  return (
-    <div className="flex flex-row gap-4 items-center py-2">
-      <p className="text-2xl font-semibold">{title}</p>
-      <ScoreBadge score={categoryScore} />
-    </div>
-  );
-};
-
 const ScoreBadge = ({ score }: { score: number }) => {
   return (
     <div
@@ -105,6 +35,21 @@ const ScoreBadge = ({ score }: { score: number }) => {
       >
         {score}/100
       </p>
+    </div>
+  );
+};
+
+const CategoryHeader = ({
+  title,
+  categoryScore,
+}: {
+  title: string;
+  categoryScore: number;
+}) => {
+  return (
+    <div className="flex flex-row gap-4 items-center py-2">
+      <p className="text-2xl font-semibold">{title}</p>
+      <ScoreBadge score={categoryScore} />
     </div>
   );
 };
@@ -160,3 +105,58 @@ const CategoryContent = ({
     </div>
   );
 };
+
+const Details = ({ feedback }: { feedback: Feedback }) => {
+  return (
+    <div className="flex flex-col gap-4 w-full">
+      <Accordion>
+        <AccordionItem id="tone-style">
+          <AccordionHeader itemId="tone-style">
+            <CategoryHeader
+              title="Tone & Style"
+              categoryScore={feedback.toneAndStyle.score}
+            />
+          </AccordionHeader>
+          <AccordionContent itemId="tone-style">
+            <CategoryContent tips={feedback.toneAndStyle.tips} />
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem id="content">
+          <AccordionHeader itemId="content">
+            <CategoryHeader
+              title="Content"
+              categoryScore={feedback.content.score}
+            />
+          </AccordionHeader>
+          <AccordionContent itemId="content">
+            <CategoryContent tips={feedback.content.tips} />
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem id="structure">
+          <AccordionHeader itemId="structure">
+            <CategoryHeader
+              title="Structure"
+              categoryScore={feedback.structure.score}
+            />
+          </AccordionHeader>
+          <AccordionContent itemId="structure">
+            <CategoryContent tips={feedback.structure.tips} />
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem id="skills">
+          <AccordionHeader itemId="skills">
+            <CategoryHeader
+              title="Skills"
+              categoryScore={feedback.skills.score}
+            />
+          </AccordionHeader>
+          <AccordionContent itemId="skills">
+            <CategoryContent tips={feedback.skills.tips} />
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+    </div>
+  );
+};
+
+export default Details;
