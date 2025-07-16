0144baa382dee9a9afc8fd5c6d736ee5a2c8cab6
Darvin <darwindpatel@gmail.com>
2025-07-16 21:19:32 +0200

    fix: unecessary double check for image

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -45,13 +45,11 @@ const ResumeCard = ({ resume }: { resume: Resume }) => {
       {resumeUrl && (
         <div className="gradient-border animate-in fade-in duration-1000">
           <div className=" w-full h-full">
-            {resumeUrl && (
-              <img
-                src={resumeUrl}
-                alt="resume"
-                className="w-full h-[350px] max-sm:h-[200px] object-cover object-top"
-              />
-            )}
+            <img
+              src={resumeUrl}
+              alt="resume"
+              className="w-full h-[350px] max-sm:h-[200px] object-cover object-top"
+            />
           </div>
         </div>
       )}
